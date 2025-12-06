dfd895edade21b3d653209bac33697218558c9b8
José Manuel Jiménez Rodríguez <josemajimenezrodriguez8@gmail.com>
2025-12-07 00:26:28 +0100

    Añadido svelte a Astro para usar Github Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-// ecentia/evatorresmoda-resources/evatorresmoda-Resources-master/astro.config.mjs
-
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
+import svelte from '@astrojs/svelte';
 import sanity from '@sanity/astro';
 import { loadEnv } from "vite";
 
@@ -14,23 +13,24 @@ const DATASET = process.env.PUBLIC_SANITY_DATASET || env.PUBLIC_SANITY_DATASET;
 
 export default defineConfig({
   site: 'https://www.evatorresmoda.com',
-  // IMPORTANTE: No activar 'base' si usas dominio propio (evatorresmoda.com)
   
+  
+  output: 'static',
+
   integrations: [
     sitemap(),
     react(),
+    svelte(), 
     sanity({
       projectId: PROJECT_ID,
       dataset: DATASET,
       useCdn: false,
       apiVersion: '2024-03-01',
-      // CORRECCIÓN: Forzamos el path /admin para que se compile también en producción/build.
       studioBasePath: '/admin', 
     }),
   ],
+
   vite: {
     plugins: [tailwindcss()]
-  },
-  // Esto asegura que Astro sepa que es una web estática
-  output: 'static'
+  }
 });
