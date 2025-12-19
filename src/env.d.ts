05f6d594073797197306cfcda406701bfd2d5d37
José Manuel Jiménez Rodríguez <josemajimenezrodriguez8@gmail.com>
2025-12-19 13:43:30 +0100

    Implementas las Cookies de Google Analytics

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,9 @@
-// src/env.d.ts
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 /// <reference types="@sanity/astro/module" />
-// El compilador de TypeScript ahora debe encontrar los tipos a través de la línea de arriba.
 
+interface Window {
+  loadGoogleAnalytics: () => void;
+  gaLoaded?: boolean;
+  dataLayer?: any[];
+}
